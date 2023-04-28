ecd260c537980175e4b01bc8049f2b332801fe7e
abdullahadam2003 <abdullahadam2017@gmail.com>
2023-04-28 11:56:44 +0700

    upload latihan modul teori

---
+++ src/main/java/modul3/latihanmodul31_Helloworld.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul3;
+
+/**
+ *
+ * @author windows
+ */
+public class latihanmodul31_Helloworld {
+    public static void main(String[] args ){
+       System.out.println("Welcome To Java Programming ABDULLAH ADAM");
+}
+}
